da3c3102f60fa854c95fcc5c7be39ab3ed630bab
Amr Naser <wael01148799458@gmail.com>
2025-03-20 05:20:21 +0200

    i wanna kill myself

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
-from burn_detect_api.app import create_app
+from burn_detect_api import app
 
-flask_app = create_app()
+flask_app = app.create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Railway provides a port
