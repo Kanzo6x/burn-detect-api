6b8186af40c998902501b7473ad0fb3e2f13ae8b
Amr Naser <wael01148799458@gmail.com>
2025-03-22 17:31:24 +0200

    lol

--- run.py
+++ run.py
@@ -3,16 +3,10 @@ from burn_detect_api import app
 
 flask_app = app.create_app()
 
-# Add a test route to verify the server is running
-@flask_app.route('/')
-def home():
-    return 'Burn Detection API is running!'
+@flask_app.errorhandler(500)
+def handle_500(e):
+    return {'error': 'Internal Server Error'}, 500
 
 if __name__ == "__main__":
-    # Development server
     port = int(os.environ.get("PORT", 5000))
     flask_app.run(host="0.0.0.0", port=port)
-else:
-    # Production server (Gunicorn will use this)
-    port = int(os.environ.get("PORT", 8000))
-    flask_app.config['SERVER_NAME'] = f"0.0.0.0:{port}"
