527e7347f8535ba9f1de5b1949797a4dcfd1d2ff
Amr Naser <wael01148799458@gmail.com>
2025-03-22 16:41:44 +0200

    lol

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from burn_detect_api import app
 flask_app = app.create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Railway provides a port
+    port = int(os.environ.get("PORT", 5000))  
     flask_app.run(host="0.0.0.0", port=port)
