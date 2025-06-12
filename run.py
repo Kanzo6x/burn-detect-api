5e7109528c7ed9059c313af6c1abc38b0eb98027
Amr Naser <wael01148799458@gmail.com>
2025-06-12 22:36:09 +0300

    changing the port number to 5001

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ def handle_500(e):
     return {'error': 'Internal Server Error'}, 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     flask_app.run(host="0.0.0.0", port=port)
